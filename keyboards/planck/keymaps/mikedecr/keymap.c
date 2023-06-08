4922ab13ca9308906e710d175144a8128d6bccf4
mikedecr <mgdecrescenzo@gmail.com>
2023-06-07 22:07:58 -0500

    layer-tap raise/lower
    raise and lower still have the same layer capability, but put - and = as taps respectively

--- keyboards/planck/keymaps/mikedecr/keymap.c
+++ keyboards/planck/keymaps/mikedecr/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    LT(_SPACEFN, KC_F),    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    BACKLIT,         KC_LCTL, KC_LALT, KC_LGUI, LOWER,   _______,  LT(_SPACEFN, KC_SPC),      RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    BACKLIT,         KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER, KC_EQL),  _______,  LT(_SPACEFN, KC_SPC),   LT(RAISE, KC_MINS),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_SPACEFN] = LAYOUT_planck_grid(
