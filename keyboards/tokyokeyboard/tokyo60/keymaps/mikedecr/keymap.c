21627f85904088daafe6a164e77f2c1121125d18
mdecrescenzo <mdecrescenzo@drwhholdings.com>
2026-02-20 11:37:59 -0600

    right-hand GUI holds

--- keyboards/tokyokeyboard/tokyo60/keymaps/mikedecr/keymap.c
+++ keyboards/tokyokeyboard/tokyo60/keymaps/mikedecr/keymap.c
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BL] = LAYOUT_60_hhkb(
         KC_LGUI,    KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_GRV,
         KC_TAB,    KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,
-        LCTL_T(KC_ESC),  KC_A,    KC_S,   KC_D,   LT(FN, KC_F),   KC_G,   KC_H,   LGUI_T(KC_J),   KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
+        LCTL_T(KC_ESC),  KC_A,    KC_S,   KC_D,   LT(FN, KC_F),   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, RGUI_T(KC_ENT),
         LSFT_T(KC_EQL),            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,      KC_RSFT, MO(FN),
-             KC_LALT, LGUI_T(KC_EQL),              LT(SPACEFN, KC_SPC),                         MO(FN), KC_RALT ),
+             KC_LALT, LGUI_T(KC_EQL),              LT(SPACEFN, KC_SPC),                         KC_LGUI, KC_RALT ),
     /* 1: HHKB Fn layer
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ Ins │ Del │
