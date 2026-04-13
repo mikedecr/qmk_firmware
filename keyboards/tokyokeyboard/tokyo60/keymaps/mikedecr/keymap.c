7c1b316fb3142d6fe90bda954bee87893357ecc6
mikedecr <mgdecrescenzo@gmail.com>
2026-04-12 19:26:19 -0500

    spacefn stash

--- keyboards/tokyokeyboard/tokyo60/keymaps/mikedecr/keymap.c
+++ keyboards/tokyokeyboard/tokyo60/keymaps/mikedecr/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,
         LCTL_T(KC_ESC),  KC_A,    KC_S,   KC_D,   LT(FN, KC_F),   KC_G,   KC_H,   LT(FN, KC_J),   KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
         KC_LSFT,            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,      KC_RSFT, MO(FN),
-             KC_LALT, KC_LCTL,              LT(NUMS, KC_SPC),                         KC_RGUI, KC_RALT ),
+             KC_LALT, KC_LGUI,              LT(FN, KC_SPC),                         KC_RGUI, KC_RALT ),
     /* 1: HHKB Fn layer
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ Ins │ Del │
